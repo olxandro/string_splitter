17c37bcafe6d9b9deae1db1843f94138eeb388a5
Admin O <olexandro@gmail.com>
2023-03-07 17:21:28 +0300

    Описание условия.

--- src/main/java/com/skypro/olexandro/Main.java
+++ src/main/java/com/skypro/olexandro/Main.java
@@ -1,4 +1,6 @@
-package com.skypro.olexandro;public class Main {
+package com.skypro.olexandro;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
